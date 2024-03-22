47b5799b2c24a06d82696940d48d46f47b85dba5
Camilo Ñanco <71110949+camilo09m@users.noreply.github.com>
2024-03-22 10:01:30 -0300

    Add files via upload

--- problema3.py
+++ problema3.py
@@ -9,5 +9,24 @@
 #         20 30 90 90 8 5 90
 #La salida debe ser
 #         [20, [8, [5, [], [], []], [], []], [], [30, [], [], [90, [], [90, [], [90, [], [], []], []], []]]]
-t = input()
-print(t)
+def arbolBinario(numero):
+	return [numero, [], [], []]
+
+def insertaEnArbolBinario(arbol, numero):
+    if arbol == []:
+        arbol += arbolBinario(numero)
+    else:
+        if numero < arbol[0]:
+            insertaEnArbolBinario(arbol[1], numero)
+        elif arbol[0] == numero:
+            insertaEnArbolBinario(arbol[2], numero)
+        else:
+            insertaEnArbolBinario(arbol[3], numero)
+
+
+entrada = input().split()
+entrada = [int(x) if x.isdigit() else x for x in entrada]
+w=arbolBinario(entrada[0])
+for i in range(1, len(entrada)):
+    insertaEnArbolBinario(w, entrada[i])
+print(w)
